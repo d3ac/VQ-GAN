6d52a3be76bfa65c0874928fea1cfe3fa8567601
d3ac <1048563083@qq.com>
2024-12-17 16:37:17 +0800

    重构 VQGAN 类，整合编码器、解码器和代码本模块，添加前向传播和权重调整逻辑

--- vqgan.py
+++ vqgan.py
@@ -1,120 +1,52 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-
-class GroupNorm(nn.Module):
-    def __init__(self, channels):
-        super(GroupNorm, self).__init__()
-        self.gn = nn.GroupNorm(num_groups=32, num_channels=channels, eps=1e-6, affine=True)
-
-    def forward(self, x):
-        return self.gn(x)
-
-class Swish(nn.Module):
-    def forward(self, x):
-        return x * torch.sigmoid(x)
-
-class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(ResBlock, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.block = nn.Sequential(
-            GroupNorm(in_channels),
-            Swish(),
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
-            GroupNorm(out_channels),
-            Swish(),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1)
-        )
-
-        if in_channels != out_channels:
-            self.channel_up = nn.Conv2d(in_channels, out_channels, 1, 1, 0)
-
-    def forward(self, x):
-        if self.in_channels != self.out_channels:
-            return self.channel_up(x) + self.block(x)
-        else:
-            return x + self.block(x)
-
-class NonLocalBlock(nn.Module):
-    def __init__(self, channels):
-        super(NonLocalBlock, self).__init__()
-        self.in_channels = channels
-
-        self.gn = GroupNorm(channels)
-        self.q = nn.Conv2d(channels, channels, 1, 1, 0)
-        self.k = nn.Conv2d(channels, channels, 1, 1, 0)
-        self.v = nn.Conv2d(channels, channels, 1, 1, 0)
-        self.proj_out = nn.Conv2d(channels, channels, 1, 1, 0)
-
-    def forward(self, x):
-        h_ = self.gn(x)
-        q = self.q(h_)
-        k = self.k(h_)
-        v = self.v(h_)
-
-        b, c, h, w = q.shape
-
-        q = q.reshape(b, c, h*w)
-        q = q.permute(0, 2, 1)
-        k = k.reshape(b, c, h*w)
-        v = v.reshape(b, c, h*w)
-
-        attn = torch.bmm(q, k)
-        attn = attn * (int(c)**(-0.5))
-        attn = F.softmax(attn, dim=2)
-        attn = attn.permute(0, 2, 1)
-
-        A = torch.bmm(v, attn)
-        A = A.reshape(b, c, h, w)
-
-        return x + A
-
-class codebook(nn.Module):
-    def __init__(self, args):
-        super(codebook, self).__init__()
-        self.num_codebook_vectors = args.num_codebook_vectors
-        self.latent_dim = args.latent_dim
-        self.beta = args.beta
-        self.embedding = nn.Embedding(self.num_codebook_vectors, self.latent_dim)
-        self.embedding.weight.data.uniform_(-1/self.num_codebook_vectors, 1/self.num_codebook_vectors) # 这样初始化，不然太大了全部加起来直接炸了
-
-    def forward(self, z):
-        """
-        公式图片 : https://cdn.mathpix.com/snip/images/0-890j6AncvDWQRfJwcg3vLt07dCZHbR3X8YYCq7JuQ.original.fullsize.png
-        """
-        z = z.permute(0, 2, 3, 1).contiguous() # 把channel放到最后，在这里也就是隐藏层latent_dim
-        z_flat = z.view(-1, self.latent_dim)
-        dist = torch.sum(z_flat**2, dim=1, keepdim=True) + torch.sum(self.embedding.weight**2, dim=1) - 2 * torch.matmul(z_flat, self.embedding.weight.t())
-        min_encoding_indices = torch.argmin(dist, dim=1)
-        z_q = self.embedding(min_encoding_indices).view(z.shape)
-        loss = self.beta * torch.mean((z_q.detach() - z) ** 2) + torch.mean((z_q - z.detach()) ** 2) 
-        z_q = z + (z_q - z).detach()
-
-class Encoder(nn.Module):
-    def __init__(self, args):
-        super(Encoder, self).__init__()
-        channels = args.channels
-        attn_resolutions = [16]
-        latent_dim = args.latent_dim
-        resolution = args.resolution
-        num_res_blocks = 2
-        layers = [nn.Conv2d(args.image_channels, channels[0], 3, 1, 1)]
-        for i in range(len(channels) - 1):
-            in_channels = channels[i]
-            out_channels = channels[i + 1]
-            for j in range(num_res_blocks):
-                layers.append(ResBlock(in_channels, out_channels))
-                in_channels = out_channels
-                if resolution in attn_resolutions:
-                    layers.append(NonLocalBlock(in_channels))
-            if resolution > latent_dim:
-                layers.append(DownSampleBlock(channels[i+1]))
-                resolution //= 2
-        
+from utils import ResBlock, NonLocalBlock, DownSampleBlock, Swish
+from encoder import Encoder
+from decoder import Decoder
+from codebook import Codebook
 
 
 class VQGAN(nn.Module):
     def __init__(self, args):
         super(VQGAN, self).__init__()
+        self.encoder = Encoder(args).to(device=args.device)
+        self.decoder = Decoder(args).to(device=args.device)
+        self.codebook = Codebook(args).to(device=args.device)
+        self.quant_conv = nn.Conv2d(args.latent_dim, args.latent_dim, 1).to(device=args.device) # 1x1卷积参数只有两个参数w和b
+        self.post_quant = nn.Conv2d(args.latent_dim, args.latent_dim, 1).to(device=args.device)
+
+    def forward(self, imgs):
+        encoded = self.encoder(imgs)
+        quantized = self.quant_conv(encoded)
+        zq, indices, loss = self.codebook(quantized)
+        post_quant = self.post_quant(zq)
+        decoded = self.decoder(post_quant)
+        return decoded, indices, loss
+
+    def encode(self, imgs):
+        encoded = self.encoder(imgs)
+        quantized = self.quant_conv(encoded)
+        zq, indices, loss = self.codebook(quantized)
+        return zq, indices, loss
+    
+    def decode(self, z):
+        pass
+
+    def calculate_lambda(self, perceptual_loss, gan_loss):
+        last_layer = self.decoder.model[-1]
+        last_layer_weight = last_layer.weight
+        perceptual_loss_grads = torch.autograd.grad(perceptual_loss, last_layer_weight, retain_graph=True)[0]
+        gan_loss_grads = torch.autograd.grad(gan_loss, last_layer_weight, retain_graph=True)[0]
+        λ = torch.norm(perceptual_loss_grads) / (torch.norm(gan_loss_grads) + 1e-4)
+        λ = torch.clamp(λ, 0, 1e4).detach()
+        return 0.8 * λ
+    
+    @staticmethod
+    def adopt_weight(disc_factor, i, threshold, value=0):
+        if i < threshold:
+            disc_factor = value
+        return disc_factor
+
+    def load_checkpoint(self, path):
+        self.load_state_dict(torch.load(path, weights_only=True))
